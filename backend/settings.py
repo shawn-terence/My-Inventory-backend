299f0b34e8ad7a644cbf04fab935bb24924f7f8c
terence <terenceshawn18@outlook.com>
2024-08-23 17:47:32 +0300

    Fix:Fixed module issue

--- backend/settings.py
+++ backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middlewear.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "backend.urls"
