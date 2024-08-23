52b62e106596b1857468c9e3d5b01520f4167070
terence <terenceshawn18@outlook.com>
2024-08-23 17:22:17 +0300

    Feat:created User Sign Up View

--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from django.db import transaction
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import authenticate, login
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+from .forms import UploadFileForm
+from django.db import transaction as db_transaction
+from datetime import datetime
+import pandas as pd
 
 # Create your views here.
+User=get_user_model()
+
+#User registration 
+class UserRegistrationView(APIView):
+    def post(self,request):
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#User login
