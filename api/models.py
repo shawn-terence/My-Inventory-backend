fe302e02a7d726b35f239a6e8c59eaf86ee5e96c
terence <terenceshawn18@outlook.com>
2024-08-23 17:01:47 +0300

    Feat:Added Inventory model

--- api/models.py
+++ api/models.py
@@ -34,3 +34,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email 
+
+class Inventory(models.model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
