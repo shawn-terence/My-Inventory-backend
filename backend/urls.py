dae75df5ae6ef4ece96c06ccb9d330354b562519
terence <terenceshawn18@outlook.com>
2024-08-23 17:57:48 +0300

    Feat:Added view endpoints to urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,23 +1,17 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.contrib import admin
 from django.urls import path
-
+from api.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/",UserRegistrationView.as_view(),name="register"),
+    path("login/",UserLoginView.as_view(),name="login"),
+    path("inventory/add/",AddInventoryView.as_view(),name="add_asset"),
+    path("inventory/",InventoryListView.as_view(),name="list_asset"),
+    path("buy/",PurchaseView.as_view(),name="buy"),
+    path("update/<int:pk>/add/",UpdateInventoryView.as_view(),name="add_stock"),
+    path('upload/', upload_file_view, name='upload_file'),
+    path("inventory/<int:pk>/delete",DeleteInventoryView.as_view(),name="delete_item"),
+    path("purchase/",PurchaseView.as_view(),name="purchase"),
+    path("transactions/",TransactionsView.as_view(),name="transactions")
 ]
